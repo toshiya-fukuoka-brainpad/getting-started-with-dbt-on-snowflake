3647f7dc68abe66a046672a803e42a0b5892aa77
toshiya-fukuoka-brainpad <toshiya.fukuoka@brainpad.co.jp>
2026-01-29 14:49:44 +0900

    Create oracle_datetime_test.sql

---
+++ tasty_bytes_dbt_demo/models/oracle_datetime_test.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized='incremental',
+    database='SILVER',
+    schema='CLN',
+    alias='TEST_ORACLE_DATETIME',
+    incremental_strategy='append'
+) }}
+
+select
+id,
+CONVERT_TIMEZONE('Asia/Tokyo','UTC',ORACLE_DATE::TIMESTAMP_NTZ)::TIMESTAMP_TZ as oracle_date,
+CONVERT_TIMEZONE('Asia/Tokyo','UTC',ORACLE_TS::TIMESTAMP_NTZ)::TIMESTAMP_TZ as oracle_ts,
+CONVERT_TIMEZONE('Asia/Tokyo','UTC',ORACLE_TS_6::TIMESTAMP_NTZ)::TIMESTAMP_TZ as oracle_ts_6,
+CONVERT_TIMEZONE('Asia/Tokyo','UTC',ORACLE_TS_9::TIMESTAMP_NTZ)::TIMESTAMP_TZ as oracle_ts_9,
+CURRENT_TIMESTAMP() as UPDATE_TIME
+from BRONZE.RAW.TEST_ORACLE_DATETIME
