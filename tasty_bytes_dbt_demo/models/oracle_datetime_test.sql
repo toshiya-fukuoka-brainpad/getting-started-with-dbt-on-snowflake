28b881c44629679ea830e3a822c7d46f5567eb70
toshiya-fukuoka-brainpad <toshiya.fukuoka@brainpad.co.jp>
2026-02-03 12:31:12 +0900

    Update oracle_datetime_test.sql
    test

--- tasty_bytes_dbt_demo/models/oracle_datetime_test.sql
+++ tasty_bytes_dbt_demo/models/oracle_datetime_test.sql
@@ -1,16 +1,24 @@
+tags=['oracle', 'incremental']
 {{ config(
     materialized='incremental',
     database='SILVER',
     schema='CLN',
     alias='TEST_ORACLE_DATETIME',
-    incremental_strategy='append'
+    incremental_strategy='append',
+    pre_hook=[
+        "ALTER SESSION SET TIMEZONE = 'Asia/Tokyo'"
+    ],
+    tags=['oracle', 'incremental']
 ) }}
 
-select
-id,
-CONVERT_TIMEZONE('Asia/Tokyo','UTC',ORACLE_DATE::TIMESTAMP_NTZ)::TIMESTAMP_TZ as oracle_date,
-CONVERT_TIMEZONE('Asia/Tokyo','UTC',ORACLE_TS::TIMESTAMP_NTZ)::TIMESTAMP_TZ as oracle_ts,
-CONVERT_TIMEZONE('Asia/Tokyo','UTC',ORACLE_TS_6::TIMESTAMP_NTZ)::TIMESTAMP_TZ as oracle_ts_6,
-CONVERT_TIMEZONE('Asia/Tokyo','UTC',ORACLE_TS_9::TIMESTAMP_NTZ)::TIMESTAMP_TZ as oracle_ts_9,
-CURRENT_TIMESTAMP() as UPDATE_TIME
-FROM {{ ref('oracle_datetime_ref') }}
+select  
+{{ convert_tz('jst_ntz_date', source_tz='Asia/Tokyo', target_tz='Asia/Tokyo') }} as jst_ntz_date,
+{{ convert_tz('jst_ntz_timestamp', source_tz='Asia/Tokyo', target_tz='Asia/Tokyo') }} as jst_ntz_timestamp,
+{{ convert_tz('jst_tz_timestamp_tz', source_tz='Asia/Tokyo', target_tz='Asia/Tokyo') }} as jst_tz_timestamp_tz,
+{{ convert_tz('jst_ltz_timestamp_ltz', source_tz='Asia/Tokyo', target_tz='Asia/Tokyo') }} as jst_ltz_timestamp_ltz,
+{{ convert_tz('utc_ntz_date', source_tz='UTC', target_tz='Asia/Tokyo') }} as utc_ntz_date,
+{{ convert_tz('utc_ntz_timestamp', source_tz='UTC', target_tz='Asia/Tokyo') }} as utc_ntz_timestamp,
+{{ convert_tz('utc_tz_timestamp_tz', source_tz='UTC', target_tz='Asia/Tokyo') }} as utc_tz_timestamp_tz,
+{{ convert_tz('utc_ltz_timestamp_ltz', source_tz='UTC', target_tz='Asia/Tokyo') }} as utc_ltz_timestamp_ltz,
+CURRENT_TIMESTAMP()  as  UPDATE_DATE
+from BRONZE.RAW.TEST_ORACLE_DATETIME
